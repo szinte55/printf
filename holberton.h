2f7606cfd26f42711ae20b7df1e6347fe47cc985
szinte55 <szinte55@gmail.com>
2019-03-18 21:53:18 +0000

    header file

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int print_signed_int(signed int);
+int print_unsigned_int(unsigned int);
+int print_unsigned_octal(unsigned int);
+int print_unsigned_binary(unsigned int);
+int print_unsigned_hex(unsigned int);
+char *str(int num, char *str);
+
+
+#endif
