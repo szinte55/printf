ef4c9c89ae11240f65f0bdb14ababfcdf72bc4df
747-diego <725@holbertonschool.com>
2019-03-20 02:54:10 +0000

    Updated holberton.h file

---
+++ holberton.h
@@ -0,0 +1,26 @@
+#ifndef HolbertonGroup
+#define HolbertonGroup
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+
+typedef struct PrintStruct
+{
+	char Structure;
+	int (*P)(va_list);
+} PointingTo;
+
+
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+int PrintOPTc(va_list OfArguments);
+int PrintOPTs(va_list OfArguments);
+int PrintOPTp(va_list OfArguments);
+int PrintOPTd(va_list OfArguments);
+int PrintOPTi(va_list OfArguments);
+
+#endif
